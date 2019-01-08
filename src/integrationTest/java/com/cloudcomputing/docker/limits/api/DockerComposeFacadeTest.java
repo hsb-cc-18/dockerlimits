f95bb8307f3d2297536a2b531db5c9d4eebdb77c
Christian Zöller <czoeller@stud.hs-bremen.de>
2019-01-08 18:35:53 +0100

    Implemented Facade

--- src/integrationTest/java/com/cloudcomputing/docker/limits/api/DockerComposeFacadeTest.java
+++ src/integrationTest/java/com/cloudcomputing/docker/limits/api/DockerComposeFacadeTest.java
@@ -1,25 +1,47 @@
 package com.cloudcomputing.docker.limits.api;
 
+import com.cloudcomputing.docker.limits.io.DockerComposeReader;
+import com.cloudcomputing.docker.limits.io.DockerComposeWriter;
+import com.cloudcomputing.docker.limits.services.compose.DockerComposeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DockerComposeFacadeTest {
 
+    @SpyBean
+    private DockerComposeReader dockerComposeReader;
+    @SpyBean
+    private DockerComposeWriter dockerComposeWriter;
+    @MockBean
+    private DockerComposeService dockerComposeServiceMock;
     @Autowired
-    DockerComposeFacade dockerComposeFacade;
+    private DockerComposeFacade dockerComposeFacade;
 
     @Test
-    public void startDockerComposeFile() {
-
+    public void startDockerComposeFile() throws IOException {
         final File dockerComposeFile = new File(getClass().getResource("../io/docker-compose-with-username.yml").getFile());
 
         dockerComposeFacade.startDockerComposeFile(dockerComposeFile);
+
+        // Verify Interaction with spies: they call the real method
+        verify(dockerComposeReader, times(1)).read(any());
+        verifyNoMoreInteractions(dockerComposeReader);
+        verify(dockerComposeWriter, times(1)).write(any(), any());
+        verifyNoMoreInteractions(dockerComposeWriter);
+        // Verify Interaction with mock: they don't call the real method
+        verify(dockerComposeServiceMock, times(1)).startComposeFile(any());
+        verifyNoMoreInteractions(dockerComposeServiceMock);
     }
 }
--- src/main/java/com/cloudcomputing/docker/limits/api/DockerComposeFacade.java
+++ src/main/java/com/cloudcomputing/docker/limits/api/DockerComposeFacade.java
@@ -2,31 +2,55 @@ package com.cloudcomputing.docker.limits.api;
 
 import com.cloudcomputing.docker.limits.io.DockerComposeReader;
 import com.cloudcomputing.docker.limits.io.DockerComposeWriter;
+import com.cloudcomputing.docker.limits.model.io.DockerCompose;
 import com.cloudcomputing.docker.limits.services.compose.DockerComposeService;
+import com.github.rozidan.springboot.logger.Loggable;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
+@Loggable
 public class DockerComposeFacade {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
     private final DockerComposeReader dockerComposeReader;
     private final DockerComposeWriter dockerComposeWriter;
     private final DockerComposeService dockerComposeService;
 
     @Autowired
     public DockerComposeFacade(DockerComposeReader dockerComposeReader, DockerComposeWriter dockerComposeWriter, DockerComposeService dockerComposeService) {
-        this.dockerComposeService = dockerComposeService;
         this.dockerComposeReader = dockerComposeReader;
         this.dockerComposeWriter = dockerComposeWriter;
+        this.dockerComposeService = dockerComposeService;
     }
 
     public void startDockerComposeFile(File dockerComposeFile) {
-        //TODO: 1 Read DockerCompose object with dockerComposeReader
-        //TODO: 2 Use hsbUsername from dockerCompose object and check that all services tagged with the username tag. Fix if required
-        //TODO: 3 Write new DockerCompose file with dockerComposeWriter (newDockerComposeFile.yml)
-        //TODO: 4 Pass the File to the dockerComposeService
+        try {
+            final DockerCompose dockerCompose = readDockerCompose(dockerComposeFile);
+            final File usersLatestDockerCompose = new File(FileUtils.getTempDirectory(), dockerCompose.getHsbUsername());
+            //TODO: validate labels
+            writeAndStartUsersDockerCompose(dockerCompose, usersLatestDockerCompose);
+        } catch (IOException e) {
+            logger.error("IO Failure", e);
+        }
     }
+
+    private void writeAndStartUsersDockerCompose(DockerCompose dockerCompose, File usersLatestDockerCompose) throws IOException {
+        dockerComposeWriter.write(usersLatestDockerCompose, dockerCompose);
+        dockerComposeService.startComposeFile(usersLatestDockerCompose);
+    }
+
+    private DockerCompose readDockerCompose(File dockerComposeFile) throws IOException {
+        final InputStream dockerComposeYML = FileUtils.openInputStream(dockerComposeFile);
+        return dockerComposeReader.read(dockerComposeYML);
+    }
+
 
 }
