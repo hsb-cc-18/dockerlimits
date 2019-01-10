6930aa2803044b83c12611e819df549e0b27673c
Christian Zöller <noex_home@yahoo.de>
2019-01-10 21:29:00 +0100

    Splitted test

--- src/integrationTest/java/com/cloudcomputing/docker/limits/ApplicationTestIT.java
+++ src/integrationTest/java/com/cloudcomputing/docker/limits/ApplicationTestIT.java
@@ -1,16 +1,23 @@
 package com.cloudcomputing.docker.limits;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ApplicationTestIT {
+
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
 	public void contextLoads() {
+		Assertions.assertThat(context).isNotNull();
 	}
 
 }
