a7aaa70ba9ddf77daa4d4cb8b05982121891f553
Eike <eike.bierden@arcor.de>
2019-01-14 18:00:04 +0100

    Config with tests

--- src/main/java/com/cloudcomputing/docker/limits/model/config/Config.java
+++ src/main/java/com/cloudcomputing/docker/limits/model/config/Config.java
@@ -1,10 +1,33 @@
 package com.cloudcomputing.docker.limits.model.config;
 
+//import com.sun.istack.internal.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
-@ConfigurationProperties("app") // prefix app, find app.* values
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix="resources") // prefix resources, find resources.* values
 public class Config {
 
+    //@NotNull
+    private int mem_limit;
+    //@NotNull
+    private int cpu_percent;
+
+    public int getMem_limit(){
+        return this.mem_limit;
+    }
+    public int getCpu_percent(){
+        return this.cpu_percent;
+    }
+
+    public void setMem_limit(int mem_limit){
+        this.mem_limit = mem_limit;
+    }
+
+    public void setCpu_percent(int cpu_percent) {
+        this.cpu_percent = cpu_percent;
+    }
 }
---
+++ src/test/java/com/cloudcomputing/docker/limits/model/ConfigTest.java
@@ -0,0 +1,62 @@
+package com.cloudcomputing.docker.limits.model;
+
+import com.cloudcomputing.docker.limits.io.DockerComposeReader;
+import com.cloudcomputing.docker.limits.io.DockerComposeWriter;
+import com.cloudcomputing.docker.limits.model.config.Config;
+import com.cloudcomputing.docker.limits.model.io.DockerCompose;
+import org.apache.commons.io.FileUtils;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.not;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ConfigTest {
+    //@Autowired
+    Config config ;//= new Config();
+
+
+    @Before
+    public void setUp() throws IOException {
+        this.config = new Config();
+    }
+
+
+
+    @Test
+    public void testMemLimitProperties() throws IOException {
+        assert this.config.getMem_limit() == 1024;
+    }
+
+    @Test
+    public void testCpuPercentProperties() throws IOException {
+        assert this.config.getCpu_percent() == 20;
+    }
+
+    @Test
+    public void testMemLimitGetter() throws IOException {
+        int mem_limit = 512;
+        this.config.setMem_limit(mem_limit);
+        assert this.config.getMem_limit() == mem_limit;
+    }
+
+    @Test
+    public void testCpuPercentGetter() throws IOException {
+        int cpu_percent = 25;
+        this.config.setCpu_percent(cpu_percent);
+        assert this.config.getCpu_percent() == cpu_percent;
+    }
+}
