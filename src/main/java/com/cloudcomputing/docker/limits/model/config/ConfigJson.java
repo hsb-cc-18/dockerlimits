1531cbaba7b26e474ca27153d366fc668d421b31
Eike <eike.bierden@arcor.de>
2019-01-16 16:54:23 +0100

    load  config working

--- src/main/java/com/cloudcomputing/docker/limits/model/config/ConfigJson.java
+++ src/main/java/com/cloudcomputing/docker/limits/model/config/ConfigJson.java
@@ -4,23 +4,38 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+import java.util.Map;
+
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 
 @JsonAutoDetect(fieldVisibility = NONE, getterVisibility = NONE, setterVisibility = NONE)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ConfigJson {
-    @JsonProperty("mem_limit")
-    private int mem_limit;
 
-    @JsonProperty("cpu_percent")
-    private int cpu_percent;
+    @JsonProperty("resourceLimits")
+    public Map<String, ResourcesSpec> resourceLimits;
+
+    public Map<String, ResourcesSpec> getResourceLimits() {
+        return resourceLimits;
+    }
+
+    public void setResourceLimits(Map<String, ResourcesSpec> resourceLimits) {
+        this.resourceLimits = resourceLimits;
+    }
+
+    //@JsonProperty("mem_limit")
+    private int mem_limit = 10;
+
+    //@JsonProperty("cpu_percent")
+    private Double cpu_percent = 2.5;
 
 
-    public int getCpu_percent() {
+    public Double getCpu_percent() {
         return cpu_percent;
     }
 
-    public void setCpu_percent(int cpu_percent) {
+    public void setCpu_percent(Double cpu_percent) {
         this.cpu_percent = cpu_percent;
     }
 
