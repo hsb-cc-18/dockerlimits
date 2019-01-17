b0cc9a21d9469c3d143ed33f0b8acbb55b5e0984
Christian Zöller <czoeller@stud.hs-bremen.de>
2019-01-17 20:21:27 +0100

    Added service to retrieve container details like name

---
+++ src/integrationTest/java/com/cloudcomputing/docker/limits/services/details/ContainerDetailsServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.cloudcomputing.docker.limits.services.details;
+
+import com.cloudcomputing.docker.limits.model.details.ContainerDetails;
+import com.cloudcomputing.docker.limits.services.util.ContainerIT;
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.jodatime.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {
+        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
+        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
+})
+public class ContainerDetailsServiceImplTest extends ContainerIT {
+
+    @Autowired
+    ContainerDetailsService containerDetailsService;
+
+    @Test
+    public void getContainerDetails() {
+        final ContainerDetails containerDetails = containerDetailsService.getContainerDetails(getContainerId());
+        assertThat(containerDetails).isNotNull();
+        assertThat(containerDetails.getName()).isEqualTo(container.getContainerInfo().name());
+        assertThat(containerDetails.getImage()).isEqualTo(container.getContainerInfo().image());
+        assertThat(new DateTime(containerDetails.getCreated())).isEqualToIgnoringSeconds(new DateTime(
+                container.getContainerInfo()
+                         .created()));
+    }
+}
