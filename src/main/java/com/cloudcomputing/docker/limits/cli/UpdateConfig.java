bd4788afde533dc72231f9aa285267d8ee647a66
Eike <eike.bierden@arcor.de>
2019-01-17 18:21:16 +0100

    save config cli

--- src/main/java/com/cloudcomputing/docker/limits/cli/UpdateConfig.java
+++ src/main/java/com/cloudcomputing/docker/limits/cli/UpdateConfig.java
@@ -1,13 +1,17 @@
 package com.cloudcomputing.docker.limits.cli;
 
 import com.cloudcomputing.docker.limits.model.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import java.io.IOException;
+
 @ShellComponent
 public class UpdateConfig {
-
+    private Logger logger = LoggerFactory.getLogger(getClass());
     Config config;
     @ShellMethod(key = "update-config", value = "Update config for a role")
     public void update(final String role,
@@ -19,6 +23,13 @@ public class UpdateConfig {
         if(null!=cpuPercentage){
             config.setMem_limit(cpuPercentage,role);
         }
+        try {
+            config.save();
+        } catch (IOException e) {
+            logger.error("Could not save config: ", e);
+            System.out.println("Could not save config");
+        }
+
     }
 
 }
