101cad42d56b083e0afdc7c4a05f0d1634d31c21
Markus <klugem89@web.de>
2019-01-19 19:03:13 +0100

    use cpu-shares

--- src/main/java/com/cloudcomputing/docker/limits/cli/UpdateContainer.java
+++ src/main/java/com/cloudcomputing/docker/limits/cli/UpdateContainer.java
@@ -1,5 +1,9 @@
 package com.cloudcomputing.docker.limits.cli;
 
+import com.cloudcomputing.docker.limits.ApplicationConfiguration;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.UpdateContainerCmd;
+import com.github.dockerjava.core.DockerClientImpl;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
@@ -11,13 +15,22 @@ public class UpdateContainer {
     public void update(
             final String containerId,
             @ShellOption(defaultValue = ShellOption.NULL) final String memoryLimit,
-            @ShellOption(defaultValue = ShellOption.NULL) final String cpuPercentage,
+            @ShellOption(defaultValue = ShellOption.NULL) final String cpuShares, // std. is 1024
             @ShellOption(defaultValue = ShellOption.NULL) final String bandwith) {
 
-        if(null != memoryLimit) {
-            
+        ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
+        DockerClient dockerClient = applicationConfiguration.getDockerClient();
+        UpdateContainerCmd updateContainerCmd =  dockerClient.updateContainerCmd(containerId);
+
+        if(cpuShares != null) {
+            updateContainerCmd = updateContainerCmd.withCpuShares(Integer.parseInt(cpuShares));
+        }
+        if(memoryLimit != null){
+            updateContainerCmd = updateContainerCmd.withMemory(Long.parseLong(memoryLimit));
+
         }
 
+        updateContainerCmd.exec();
     }
 
 }
