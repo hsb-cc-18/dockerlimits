0ddd8e07c659c16d1a6179bb4fa498d34c9b6a02
Christian Zöller <noex_home@yahoo.de>
2019-01-21 14:37:35 +0100

    Fixed bug not setting blkio_weight

--- src/main/java/com/cloudcomputing/docker/limits/cli/UpdateConfig.java
+++ src/main/java/com/cloudcomputing/docker/limits/cli/UpdateConfig.java
@@ -1,7 +1,6 @@
 package com.cloudcomputing.docker.limits.cli;
 
 import com.cloudcomputing.docker.limits.model.config.Config;
-import com.cloudcomputing.docker.limits.model.config.ConfigImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +27,7 @@ public class UpdateConfig {
         if(null!=cpuPercentage){
             config.setMem_limit(role,cpuPercentage);
         }
-        if(null!=cpuPercentage){
+        if(null!=blk_weight){
             config.setBlk_weight(role,Integer.parseInt(blk_weight));
         }
         try {
